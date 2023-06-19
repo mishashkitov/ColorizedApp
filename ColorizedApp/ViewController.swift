af0cad8c9c587b189eb6c9150ca78c1ba64f183d
mishashkitov <shkitovm@gmail.com>
2023-06-19 19:17:22 +0500

    add methods

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -28,56 +28,68 @@ class ViewController: UIViewController {
         colorView.layer.cornerRadius = 10
         setupSliders()
         setupAmountLabels()
+        updateColor()
+        view.backgroundColor = .systemGray6
     }
 
     //MARK: - IBAction
     @IBAction func redSliderAction() {
         redAmountLabel.text = String(Int(redSlider.value.rounded()))
+        updateColor()
     }
     
     @IBAction func greenSliderAction() {
         greenAmountLabel.text = String(Int(greenSlider.value.rounded()))
-        print("greeen")
+        updateColor()
     }
     
     @IBAction func blueSliderAction() {
         blueAmountLabel.text = String(Int(blueSlider.value.rounded()))
+        updateColor()
     }
     
     @IBAction func opacitySliderAction() {
         opacityAmountLabel.text = String(Int(opacitySlider.value.rounded()))
+        updateColor()
     }
     
     //MARK: - Private methods
     private func setupSliders() {
-        redSlider.minimumValue = 1
+        redSlider.minimumValue = 0
         redSlider.maximumValue = 255
-        redSlider.value = 50
         redSlider.minimumTrackTintColor = .red
+        redSlider.value = Float(Int.random(in: 0...255))
   
-        greenSlider.minimumValue = 1
+        greenSlider.minimumValue = 0
         greenSlider.maximumValue = 255
-        greenSlider.value = 1
         greenSlider.minimumTrackTintColor = .green
+        greenSlider.value = Float(Int.random(in: 0...255))
    
-        blueSlider.value = 1
-        blueSlider.minimumValue = 1
+        blueSlider.minimumValue = 0
         blueSlider.maximumValue = 255
         blueSlider.minimumTrackTintColor = .blue
+        blueSlider.value = Float(Int.random(in: 0...255))
     
-        opacitySlider.value = 1
-        opacitySlider.minimumValue = 1
+        opacitySlider.minimumValue = 0
         opacitySlider.maximumValue = 100
-        opacitySlider.minimumTrackTintColor = .white
+        opacitySlider.minimumTrackTintColor = .lightGray
+        opacitySlider.value = 100
     }
-    
-  
     
     private func setupAmountLabels() {
         redAmountLabel.text = String(Int(redSlider.value.rounded()))
         greenAmountLabel.text = String(Int(greenSlider.value.rounded()))
         blueAmountLabel.text = String(Int(blueSlider.value.rounded()))
         opacityAmountLabel.text = String(Int(opacitySlider.value.rounded()))
+    }
+    
+    private func updateColor() {
+        let red = CGFloat(redSlider.value / 255)
+        let green = CGFloat(greenSlider.value / 255)
+        let blue = CGFloat(blueSlider.value / 255)
+        let opacity = CGFloat(opacitySlider.value / 100)
+        
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: opacity)
     }
  
 }
